d5dc6cbde0278aa557913053aa9292682985446b
Philip Roberts <latentflip@gmail.com>
2012-06-29 16:16:12 +0100

    Bigger text

--- server.rb
+++ server.rb
@@ -5,7 +5,20 @@ Pusher.key = 'aa16fedd0ca224252c4d'
 Pusher.secret = '17b9668855f0d04bd160'
 CHANNEL = Pusher['chatroom']
 
-#CHANNEL.trigger('message', 'Hi Folks')
-CHANNEL.trigger('reload', '')
-#CHANNEL.trigger('deploy', File.read('./client.js'))
-#CHANNEL.trigger('deploy', '')
+def reload!
+  CHANNEL.trigger('eval', 'window.location.reload()')
+end
+
+def simon_says(message, percent=1)
+  CHANNEL.trigger 'eval', <<-JS
+    if (Math.random() < #{percent}) {
+      document.getElementsByTagName('h1')[0].innerHTML = '#{message}'
+    }
+  JS
+end
+
+
+simon_says '3'
+simon_says '2'
+simon_says '1'
+simon_says 'shout: hey'
